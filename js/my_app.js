e1c322d141261983d9f5394c34a06ea904911a22
thedecimal <yaser.al3shry@gmail.com>
2015-05-30 19:03:27 +0200

    add react and replace angular

---
+++ js/my_app.js
@@ -0,0 +1,56 @@
+(function(){
+	window.data = [
+	  {
+	    intro: 'Are you ready?',
+	    audio: 'sound3.wav'
+	  },{
+	    question: {
+	      audio: "Aa.wav",
+	      images: [
+	        "alph/E.jpeg",
+	        "alph/A.jpg",
+	        "alph/O.jpg",
+	        "alph/Aa.png"
+	      ],
+	      correctAnswer: 3
+	    }
+	  },{
+	    question: {
+	      text: "بَ",
+	      sounds: [
+	        "alph/ba.mp3",
+	        "alph/ta.mp3",
+	        "alph/tha.mp3",
+	        "alph/na.mp3"
+	      ],
+	      correctAnswer: 0
+	    }
+	  },{
+	    question: {
+	      text: "بَ",
+	      images: [
+	        "alph/ba.mp3",
+	        "alph/ta.mp3",
+	        "alph/tha.mp3",
+	        "alph/na.mp3"
+	      ],
+	      correctAnswer: 0
+	    }
+	  }
+	]
+
+})();
+/**
+- SlideShow
+	- Slide
+		- Question
+			- audio
+			- image
+			- text
+	- Slide
+		- audio
+		- image
+		- text
+	- Slide
+
+*/
